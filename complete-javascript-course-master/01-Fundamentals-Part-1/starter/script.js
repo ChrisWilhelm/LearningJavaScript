67cc1f2de85b46ddd0b1134781c47cb25a990df5
Chris Wilhelm <70656080+ChrisWilhelm@users.noreply.github.com>
2021-05-10 09:02:36 -0400

    first coding exercise

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,11 @@
 alert("Javascript is fun");
 console.log("hello world");
+
+const markMass = 95;//78;
+const johnMass = 85;//92;
+const markHeight = 1.88;//1.69;
+const johnHeight = 1.76;//1.95;
+const markBMI = markMass / (markHeight ** 2);
+const johnBMI = johnMass / (johnHeight ** 2);
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
