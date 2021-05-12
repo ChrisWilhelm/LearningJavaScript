170f2d99e70003147595b7afdebe833f834352d2
Chris Wilhelm <70656080+ChrisWilhelm@users.noreply.github.com>
2021-05-12 19:22:07 -0400

    finished debugging

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,29 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
+/*
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "cels",
+    value: Number(prompt("Degrees Celsius:")),
+  };
 
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());*/
+
+function printForecast(arr) {
+  if (Array.isArray(arr)) {
+    let temps = "...";
+    for (let i = 0; i < arr.length; i++) {
+      temps += ` ${arr[i]}C in ${i + 1} days ...`;
+    }
+    return temps;
+  } else {
+    return -1;
+  }
+}
+
+const arr = [17, 21, 23];
+console.log(printForecast(arr));
