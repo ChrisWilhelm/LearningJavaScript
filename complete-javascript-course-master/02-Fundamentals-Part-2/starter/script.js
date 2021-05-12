6aff9967e205554eaeb76fc209bc9ade5da87046
Chris Wilhelm <70656080+ChrisWilhelm@users.noreply.github.com>
2021-05-12 08:46:10 -0400

    arrow functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -4,7 +4,7 @@ let hasDriversLicense = false;
 const passTest = true;
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive");
-*/
+
 function logger() {
     console.log("My name is Chris");
 }
@@ -14,9 +14,38 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
     return juice;
 }
 
 console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+function calcAge1(birthYear) {
+    return 2037 - (birthYear);
+}
+const age1 = calcAge1(1991)
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+//arrow functions
+const calcAge3 = birthYear => 2037 - birthYear;
+
+console.log(calcAge3(1991));
+
+const yearsUntilRetirement = (birthYear, name) => {
+    const age = 2037 - birthYear;
+    const years = 65 - age;
+    return `${name} retires in ${years} years`;
+}
+
+console.log(yearsUntilRetirement(1980, 'Bob')); */
+
