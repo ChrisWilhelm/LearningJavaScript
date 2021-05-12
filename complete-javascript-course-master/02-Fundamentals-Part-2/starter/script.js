9745d467ba5f00bf9a224c23ead5ae9e7dd4b9bc
Chris Wilhelm <70656080+ChrisWilhelm@users.noreply.github.com>
2021-05-12 08:26:10 -0400

    learned functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,22 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive");
+*/
+function logger() {
+    console.log("My name is Chris");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(5, 0));
